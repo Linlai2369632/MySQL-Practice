c3d3b2120316b74b4b6e46e2d75eb352d01f53ad
Terry <foxwing2369632@gmail.com>
2025-02-20 01:28:05 +0800

    2025-02-20:

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,13 +52,27 @@
             this.dgvSQLData = new System.Windows.Forms.DataGridView();
             this.pbUpload = new System.Windows.Forms.Button();
             this.pbRefresh = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tbNewUser = new System.Windows.Forms.TextBox();
+            this.tbNewPassword = new System.Windows.Forms.TextBox();
+            this.pbAddNewUser = new System.Windows.Forms.Button();
+            this.dgvAllUsers = new System.Windows.Forms.DataGridView();
+            this.pbGetUsersInfo = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSQLData)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllUsers)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 72);
+            this.label1.Location = new System.Drawing.Point(382, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 27);
             this.label1.TabIndex = 0;
@@ -67,7 +81,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 114);
+            this.label2.Location = new System.Drawing.Point(677, 30);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 27);
             this.label2.TabIndex = 1;
@@ -84,7 +98,7 @@
             // 
             // tbHostIP
             // 
-            this.tbHostIP.Location = new System.Drawing.Point(154, 30);
+            this.tbHostIP.Location = new System.Drawing.Point(154, 25);
             this.tbHostIP.Name = "tbHostIP";
             this.tbHostIP.Size = new System.Drawing.Size(198, 35);
             this.tbHostIP.TabIndex = 3;
@@ -92,7 +106,7 @@
             // 
             // tbUser
             // 
-            this.tbUser.Location = new System.Drawing.Point(154, 72);
+            this.tbUser.Location = new System.Drawing.Point(452, 27);
             this.tbUser.Name = "tbUser";
             this.tbUser.Size = new System.Drawing.Size(198, 35);
             this.tbUser.TabIndex = 4;
@@ -100,7 +114,7 @@
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(154, 114);
+            this.tbPassword.Location = new System.Drawing.Point(791, 27);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.Size = new System.Drawing.Size(198, 35);
             this.tbPassword.TabIndex = 5;
@@ -108,9 +122,9 @@
             // 
             // pbConnect
             // 
-            this.pbConnect.Location = new System.Drawing.Point(389, 66);
+            this.pbConnect.Location = new System.Drawing.Point(1039, 21);
             this.pbConnect.Name = "pbConnect";
-            this.pbConnect.Size = new System.Drawing.Size(123, 44);
+            this.pbConnect.Size = new System.Drawing.Size(171, 56);
             this.pbConnect.TabIndex = 6;
             this.pbConnect.Text = "Connect";
             this.pbConnect.UseVisualStyleBackColor = true;
@@ -119,7 +133,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(29, 182);
+            this.label4.Location = new System.Drawing.Point(31, 19);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(236, 27);
             this.label4.TabIndex = 7;
@@ -128,7 +142,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(29, 233);
+            this.label5.Location = new System.Drawing.Point(31, 70);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(212, 27);
             this.label5.TabIndex = 8;
@@ -137,7 +151,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(165, 282);
+            this.label6.Location = new System.Drawing.Point(167, 119);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(76, 27);
             this.label6.TabIndex = 9;
@@ -146,7 +160,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(111, 332);
+            this.label7.Location = new System.Drawing.Point(113, 169);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(130, 27);
             this.label7.TabIndex = 10;
@@ -155,7 +169,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(149, 378);
+            this.label8.Location = new System.Drawing.Point(151, 215);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(82, 27);
             this.label8.TabIndex = 11;
@@ -164,7 +178,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(150, 434);
+            this.label9.Location = new System.Drawing.Point(152, 271);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(81, 27);
             this.label9.TabIndex = 12;
@@ -173,7 +187,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(80, 485);
+            this.label10.Location = new System.Drawing.Point(82, 322);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(161, 27);
             this.label10.TabIndex = 13;
@@ -181,7 +195,7 @@
             // 
             // tbProductID
             // 
-            this.tbProductID.Location = new System.Drawing.Point(282, 182);
+            this.tbProductID.Location = new System.Drawing.Point(284, 19);
             this.tbProductID.Name = "tbProductID";
             this.tbProductID.Size = new System.Drawing.Size(198, 35);
             this.tbProductID.TabIndex = 14;
@@ -189,7 +203,7 @@
             // 
             // tbLongitudeAndLatitude
             // 
-            this.tbLongitudeAndLatitude.Location = new System.Drawing.Point(282, 225);
+            this.tbLongitudeAndLatitude.Location = new System.Drawing.Point(284, 62);
             this.tbLongitudeAndLatitude.Name = "tbLongitudeAndLatitude";
             this.tbLongitudeAndLatitude.Size = new System.Drawing.Size(198, 35);
             this.tbLongitudeAndLatitude.TabIndex = 15;
@@ -197,7 +211,7 @@
             // 
             // tbEnergy
             // 
-            this.tbEnergy.Location = new System.Drawing.Point(282, 274);
+            this.tbEnergy.Location = new System.Drawing.Point(284, 111);
             this.tbEnergy.Name = "tbEnergy";
             this.tbEnergy.Size = new System.Drawing.Size(198, 35);
             this.tbEnergy.TabIndex = 16;
@@ -205,7 +219,7 @@
             // 
             // tbTemperature
             // 
-            this.tbTemperature.Location = new System.Drawing.Point(282, 324);
+            this.tbTemperature.Location = new System.Drawing.Point(284, 161);
             this.tbTemperature.Name = "tbTemperature";
             this.tbTemperature.Size = new System.Drawing.Size(198, 35);
             this.tbTemperature.TabIndex = 17;
@@ -213,7 +227,7 @@
             // 
             // tbVoltage
             // 
-            this.tbVoltage.Location = new System.Drawing.Point(282, 375);
+            this.tbVoltage.Location = new System.Drawing.Point(284, 212);
             this.tbVoltage.Name = "tbVoltage";
             this.tbVoltage.Size = new System.Drawing.Size(198, 35);
             this.tbVoltage.TabIndex = 18;
@@ -221,7 +235,7 @@
             // 
             // tbCurrent
             // 
-            this.tbCurrent.Location = new System.Drawing.Point(282, 426);
+            this.tbCurrent.Location = new System.Drawing.Point(284, 263);
             this.tbCurrent.Name = "tbCurrent";
             this.tbCurrent.Size = new System.Drawing.Size(198, 35);
             this.tbCurrent.TabIndex = 19;
@@ -230,7 +244,7 @@
             // tbLastUpdateTime
             // 
             this.tbLastUpdateTime.Enabled = false;
-            this.tbLastUpdateTime.Location = new System.Drawing.Point(282, 482);
+            this.tbLastUpdateTime.Location = new System.Drawing.Point(284, 319);
             this.tbLastUpdateTime.Name = "tbLastUpdateTime";
             this.tbLastUpdateTime.Size = new System.Drawing.Size(198, 35);
             this.tbLastUpdateTime.TabIndex = 20;
@@ -240,16 +254,16 @@
             this.dgvSQLData.AllowUserToAddRows = false;
             this.dgvSQLData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvSQLData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvSQLData.Location = new System.Drawing.Point(542, 30);
+            this.dgvSQLData.Location = new System.Drawing.Point(625, 96);
             this.dgvSQLData.Name = "dgvSQLData";
             this.dgvSQLData.RowHeadersWidth = 51;
             this.dgvSQLData.RowTemplate.Height = 27;
-            this.dgvSQLData.Size = new System.Drawing.Size(738, 487);
+            this.dgvSQLData.Size = new System.Drawing.Size(642, 428);
             this.dgvSQLData.TabIndex = 21;
             // 
             // pbUpload
             // 
-            this.pbUpload.Location = new System.Drawing.Point(357, 530);
+            this.pbUpload.Location = new System.Drawing.Point(359, 367);
             this.pbUpload.Name = "pbUpload";
             this.pbUpload.Size = new System.Drawing.Size(123, 44);
             this.pbUpload.TabIndex = 22;
@@ -267,28 +281,129 @@
             this.pbRefresh.UseVisualStyleBackColor = true;
             this.pbRefresh.Click += new System.EventHandler(this.pbRefresh_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(34, 83);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(560, 475);
+            this.tabControl1.TabIndex = 24;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.pbUpload);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.tbLastUpdateTime);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.tbCurrent);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.tbVoltage);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.tbTemperature);
+            this.tabPage1.Controls.Add(this.tbProductID);
+            this.tabPage1.Controls.Add(this.tbEnergy);
+            this.tabPage1.Controls.Add(this.tbLongitudeAndLatitude);
+            this.tabPage1.Location = new System.Drawing.Point(4, 36);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(552, 435);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pbGetUsersInfo);
+            this.tabPage2.Controls.Add(this.dgvAllUsers);
+            this.tabPage2.Controls.Add(this.pbAddNewUser);
+            this.tabPage2.Controls.Add(this.tbNewPassword);
+            this.tabPage2.Controls.Add(this.tbNewUser);
+            this.tabPage2.Controls.Add(this.label12);
+            this.tabPage2.Controls.Add(this.label11);
+            this.tabPage2.Location = new System.Drawing.Point(4, 36);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(552, 435);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(69, 29);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 27);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "New User";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(27, 88);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(144, 27);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "New Password";
+            // 
+            // tbNewUser
+            // 
+            this.tbNewUser.Location = new System.Drawing.Point(228, 30);
+            this.tbNewUser.Name = "tbNewUser";
+            this.tbNewUser.Size = new System.Drawing.Size(187, 35);
+            this.tbNewUser.TabIndex = 27;
+            // 
+            // tbNewPassword
+            // 
+            this.tbNewPassword.Location = new System.Drawing.Point(228, 88);
+            this.tbNewPassword.Name = "tbNewPassword";
+            this.tbNewPassword.Size = new System.Drawing.Size(187, 35);
+            this.tbNewPassword.TabIndex = 28;
+            // 
+            // pbAddNewUser
+            // 
+            this.pbAddNewUser.Location = new System.Drawing.Point(430, 56);
+            this.pbAddNewUser.Name = "pbAddNewUser";
+            this.pbAddNewUser.Size = new System.Drawing.Size(104, 41);
+            this.pbAddNewUser.TabIndex = 29;
+            this.pbAddNewUser.Text = "Add";
+            this.pbAddNewUser.UseVisualStyleBackColor = true;
+            this.pbAddNewUser.Click += new System.EventHandler(this.pbAddNewUser_Click);
+            // 
+            // dgvAllUsers
+            // 
+            this.dgvAllUsers.AllowUserToAddRows = false;
+            this.dgvAllUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAllUsers.Location = new System.Drawing.Point(32, 165);
+            this.dgvAllUsers.Name = "dgvAllUsers";
+            this.dgvAllUsers.RowHeadersWidth = 51;
+            this.dgvAllUsers.RowTemplate.Height = 27;
+            this.dgvAllUsers.Size = new System.Drawing.Size(492, 206);
+            this.dgvAllUsers.TabIndex = 30;
+            // 
+            // pbGetUsersInfo
+            // 
+            this.pbGetUsersInfo.Location = new System.Drawing.Point(322, 377);
+            this.pbGetUsersInfo.Name = "pbGetUsersInfo";
+            this.pbGetUsersInfo.Size = new System.Drawing.Size(202, 41);
+            this.pbGetUsersInfo.TabIndex = 31;
+            this.pbGetUsersInfo.Text = "Get Users Info";
+            this.pbGetUsersInfo.UseVisualStyleBackColor = true;
+            this.pbGetUsersInfo.Click += new System.EventHandler(this.pbGetUsersInfo_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1302, 586);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.pbRefresh);
-            this.Controls.Add(this.pbUpload);
             this.Controls.Add(this.dgvSQLData);
-            this.Controls.Add(this.tbLastUpdateTime);
-            this.Controls.Add(this.tbCurrent);
-            this.Controls.Add(this.tbVoltage);
-            this.Controls.Add(this.tbTemperature);
-            this.Controls.Add(this.tbEnergy);
-            this.Controls.Add(this.tbLongitudeAndLatitude);
-            this.Controls.Add(this.tbProductID);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.pbConnect);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUser);
@@ -301,6 +416,12 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgvSQLData)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllUsers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -332,6 +453,16 @@
         private System.Windows.Forms.DataGridView dgvSQLData;
         private System.Windows.Forms.Button pbUpload;
         private System.Windows.Forms.Button pbRefresh;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button pbAddNewUser;
+        private System.Windows.Forms.TextBox tbNewPassword;
+        private System.Windows.Forms.TextBox tbNewUser;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DataGridView dgvAllUsers;
+        private System.Windows.Forms.Button pbGetUsersInfo;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -28,8 +28,8 @@ namespace Mysql_test
             // 建立 Connection String
             // 請根據您本地端的 MySQL 資料庫、帳號與密碼調整
             //connectionString = $"Server={tbHostIP.Text};Database=testdb;User ID={tbUser.Text};Password={tbPassword};";
-            connectionString = $"Server={tbHostIP.Text};Database=testdb;User ID={tbUser.Text};Password={tbPassword};SslMode=None;AllowPublicKeyRetrieval=True;";
-            connectionString = $"Server=127.0.0.1;Database=testdb;User ID=root;Password=root;SslMode=None;AllowPublicKeyRetrieval=True;";
+            connectionString = $"Server={tbHostIP.Text};Database=testdb;User ID={tbUser.Text};Password={tbPassword.Text};SslMode=None;AllowPublicKeyRetrieval=True;";
+            //connectionString = $"Server=127.0.0.1;Database=testdb;User ID=root;Password=root;SslMode=None;AllowPublicKeyRetrieval=True;";
 
 
             using (MySqlConnector.MySqlConnection conn = new MySqlConnector.MySqlConnection(connectionString))
@@ -136,6 +136,99 @@ namespace Mysql_test
                         adapter.Fill(dt);
                         dgvSQLData.DataSource = dt; // 設定 DataGridView 的資料來源
                     }
+                }
+            }
+        }
+
+        #region Add New User
+        private void pbAddNewUser_Click(object sender, EventArgs e)
+        {
+            string UserName = tbUser.Text;
+            string Password = tbPassword.Text;
+
+            using (MySqlConnector.MySqlConnection conn = new MySqlConnector.MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // 建立新使用者
+                //string createUserQuery = $@"CREATE USER '{UserName}'@'localhost' IDENTIFIED BY '{Password}';"; // C# 語法問題
+                string createUserQuery = "CREATE USER '1111'@'localhost' IDENTIFIED BY '1111';";
+                using (MySqlConnector.MySqlCommand cmd = new MySqlConnector.MySqlCommand(createUserQuery, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                // 賦予權限 (全部權限)(可依實際需求調整)
+                //string grantQuery = @"GRANT ALL PRIVILEGES ON testdb.* TO 'testuser'@'localhost';";
+                // 賦予指定權限
+                string grantQuery = $@"
+                GRANT SELECT, INSERT, UPDATE, DELETE 
+                ON testdb.* 
+                TO '{UserName}'@'localhost';
+            ";
+                using (MySqlConnector.MySqlCommand cmd = new MySqlConnector.MySqlCommand(grantQuery, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                // 重新載入權限
+                string flushQuery = "FLUSH PRIVILEGES;";
+                using (MySqlConnector.MySqlCommand cmd = new MySqlConnector.MySqlCommand(flushQuery, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                conn.Close();
+            }
+
+            Console.WriteLine("新使用者建立與權限設定完成。");
+        }
+        #endregion
+
+        private async void pbGetUsersInfo_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnector.MySqlConnection conn = new MySqlConnector.MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // 這裡列出常見的 \_priv 欄位，可視需求調整
+                string sql = @"
+                        SELECT 
+                            Host,
+                            User,
+                            Select_priv,
+                            Insert_priv,
+                            Update_priv,
+                            Delete_priv,
+                            Create_priv,
+                            Drop_priv,
+                            Grant_priv,
+                            References_priv,
+                            Index_priv,
+                            Alter_priv,
+                            Show_db_priv,
+                            Super_priv,
+                            Create_tmp_table_priv,
+                            Lock_tables_priv,
+                            Execute_priv,
+                            Repl_slave_priv,
+                            Repl_client_priv,
+                            Create_view_priv,
+                            Show_view_priv,
+                            Create_routine_priv,
+                            Alter_routine_priv,
+                            Create_user_priv,
+                            Event_priv,
+                            Trigger_priv
+                        FROM mysql.user;
+                    ";
+
+                using (var cmd = new MySqlConnector.MySqlCommand(sql, conn))
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dgvAllUsers.DataSource = dt;
                 }
             }
         }
